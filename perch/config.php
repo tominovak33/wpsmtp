0ac9b3b00516d8f7779089b0c5b350ce54391597
tominovak33 <tominovak33@gmail.com>
2015-07-21 10:10:16 +0100

    all the perch goodness

---
+++ perch/config.php
@@ -0,0 +1,16 @@
+/*---------------------------
+/ SMTP
+/--------------------------*/
+define('PERCH_EMAIL_METHOD', 'smtp');
+define('PERCH_EMAIL_HOST', 'smtp.mandrillapp.com'); // Your smtp provider
+define('PERCH_EMAIL_SECURE', 'tls'); // Unlike with WP 'ssl' wont work
+define('PERCH_EMAIL_PORT', 587); // Port that your smtp provider uses
+define('PERCH_EMAIL_USERNAME', '<your-mandrill-email-username>');
+define('PERCH_EMAIL_PASSWORD', '<your-mandrill-api-key>');
+define('PERCH_EMAIL_AUTH', true); // To make it work
+
+// Make sure that your hosting provider allows outbound SMTP connections on the port you use for the user that the site is running as!
+
+/*---------------------------
+/ End
+/--------------------------*/
--- tominovak33-wp-mail-smtp/index.php
+++ tominovak33-wp-mail-smtp/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: TOMINOVAK33-WPMAIL-MANDRILL
- * Description: Set the right phpmailer settings for using mandrill SMTP servers
+ * Plugin Name: TOMINOVAK33-WPMAIL-SMTP
+ * Description: Set the right phpmailer settings for using other SMTP servers
  */
 
 defined( 'ABSPATH' ) OR exit; // Only run as part of WP
@@ -9,12 +9,12 @@ defined( 'ABSPATH' ) OR exit; // Only run as part of WP
 add_action( 'phpmailer_init', 'phpmailerSMTP' );
 
 function phpmailerSMTP( $phpmailer ) { 
-    $phpmailer->IsSMTP();                                         // Set mailer to use SMTP
-    $phpmailer->Host         = 'smtp.mandrillapp.com';            // Specify main and backup server
-    $phpmailer->Port         = 587;                               // Set the SMTP port
+    $phpmailer->IsSMTP();                                         // Set phpmailer to use SMTP
+    $phpmailer->Host         = 'smtp.mandrillapp.com';            // Specify SMTP server
+    $phpmailer->Port         = 587;                               // Set the SMTP port for your provider
     $phpmailer->SMTPAuth     = true;                              // Enable SMTP authentication
-    $phpmailer->Username     = '<USERNAME>';                      // SMTP username
-    $phpmailer->Password     = '<API-KEY>';                       // SMTP password
+    $phpmailer->Username     = '<USERNAME>';                      // SMTP username (your mandrill username for example)
+    $phpmailer->Password     = '<API-KEY>';                       // SMTP password (A valid API key from your account)
     $phpmailer->SMTPSecure   = 'tls';                             // Enable encryption, 'ssl' also accepted
     $phpmailer->addCustomHeader("X-MC-Subaccount: <SUBACCOUNT>"); // Subaccount ID from mandrill to allow for separation
 }
